14e78ec63299ebf87cd027911586c3e89fb3de60
emmashie <emmashie@users.noreply.github.com>
2017-06-09 09:46:37 -0700

    Create analysis.py

--- analysis.py
+++ analysis.py
@@ -3,6 +3,7 @@
 import numpy as np
 import scipy
 from numpy.fft import fft, ifft, fftshift, fftfreq
+from stompy.utils import model_skill
 
 def blackman_window(width=0):
     x = np.linspace(-1, 1, width + 2, endpoint=True) * np.pi
@@ -21,8 +22,7 @@ def band_avg(x=0, y=0, m=5, dt=1, window='Blackman'):
     """ x, y, m (number of subrecords), window='Blackman' or 'None'
     """
     N = len(x)
-    y_new = detrend(x, y)
-    
+    y_new = detrend(x, y)   
     if window == 'Blackman':
         weights = blackman_window(N)
         y_new = y_new * weights
@@ -30,18 +30,17 @@ def band_avg(x=0, y=0, m=5, dt=1, window='Blackman'):
         y_new = y_new
     else:
         raise ValueError("Invalid option. 'Blackman' or 'None' allowed")
-        return
-    
+        return    
     fy = fftshift(fft(y_new))
-    freqs = fftshift(fftfreq(len(x), dt))
-    
-    spectra = (abs(fy)**2 * dt)/ len(x)
-    
-    boxcar = np.ones(m)/m
-    
-    spectra_avg = np.convolve(boxcar, spectra, mode='same')
-    
-    df = 2*m
-    alpha = 0.05
-    
+    freqs = fftshift(fftfreq(len(x), dt))   
+    spectra = (abs(fy)**2 * dt)/ len(x)    
+    boxcar = np.ones(m)/m    
+    spectra_avg = np.convolve(boxcar, spectra, mode='same')       
     return freqs, spectra_avg
+
+def model_metrics(mod, obs):
+	ms = model_skill(mod, obs)
+	bias = np.mean(mod, obs)
+	r2 = np.corrcoef(mod, obs)[0,1]**2
+	rms = np.mean(np.sqrt(mod - obs)**2))
+	return ms, bias, r2, rms
