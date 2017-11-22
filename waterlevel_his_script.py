091887ed52edd515aa5e907b29e6190572acf5b0
Rusty Holleman <rustyh@sfei.org>
2017-11-22 12:55:36 -0800

    Tweak colors, analysis, sig figs, datums for water level plots

--- waterlevel_his_script.py
+++ waterlevel_his_script.py
@@ -1,15 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pyscripts.waterlevel_validation as wv
+import waterlevel_validation as wv
 from matplotlib.dates import num2date, date2num
-import pyscripts.analysis as an
+import analysis as an
 import datetime as dt
 from stompy.spatial import proj_utils
 import os
 import netCDF4 as nc
+import xarray as xr
 from stompy.io.local import noaa_coops
-import pyscripts.analysis as an
-
+from stompy import utils
 
 #hisfile = "/home/emma/sfb_dfm_setup/r14/DFM_OUTPUT_r14/his_files/r14_0000*.nc"
 path = "/opt/data/delft/sfb_dfm_v2/runs/wy2013a/DFM_OUTPUT_wy2013a/"
@@ -17,13 +17,6 @@ hisfile = path + "wy2013a_0000_20120801_000000_his.nc"
 savepath = path + "/validation_plots/waterlevel_validation_plots/"
 metpath = path + "model_metrics/"
 met = "waterlevel_metrics"
-
-f = open(metpath + met, "w")
-f.write("\\begin{center} \n")
-f.write("\\begin{adjustbox}{width=1\\textwidth} \n")
-f.write("\\begin{tabular}{| l | l | l | l | l | l |} \n")
-f.write("\\hline \n")
-f.write("Name & Skill &  Bias & \(r^2\) & RMS & Lag \\\ \\hline \n")
 
 if not os.path.exists(savepath):
 	os.makedirs(savepath)
@@ -40,162 +33,131 @@ mll = np.zeros((2, len(xcoor)))
 for i in range(len(xcoor)):
 	mll[:,i] = utm_to_ll([xcoor[i], ycoor[i]])
 
-##### station 9414290
-dat =  noaa_coops.coops_dataset_product(station="9414290", product="water_level", start_date=np.datetime64("2012-08-01"), end_date=np.datetime64("2013-09-01"), days_per_request=31)
-lon = dat["lon"].values[0,0]
-lat = dat["lat"].values[0,0]
-time = dat["time"]
-dtime = [dt.datetime.utcfromtimestamp(time[i].astype(float)/1e9) for i in range(len(time))]
-times = date2num(dtime)
-waterlevel = dat["water_level"][0,:]
-ofreq, ospec = an.band_avg(times, waterlevel, dt=times[1]-times[1])
-dist = np.sqrt((mll[0,:]-lon)**2 +  (mll[1,:]-lat)**2)
-rec = np.where(dist == np.min(dist))[0]
-mtime, mwaterlevel = wv.load_model(hisfile, rec=rec)
-mtimes = date2num(mtime)
-mwaterleveli = np.interp(times, mtimes, np.asarray(mwaterlevel)[:,0])
-mfreq, mspec = an.band_avg(mtimes, np.asarray(mwaterlevel)[:,0])
-#ms, bias, r2, rms, lag = an.model_metrics(times, mwaterleveli, times, waterlevel)
-#f.write("San Francisco & %0.4f & %0.4f & %0.4f & %0.4f & %0.4f \\\ \\hline \n" % (ms, bias, r2, rms, lag))
-# plotting
-fig, ax = plt.subplots(figsize=(6,4))
-ax.plot(time, waterlevel, color='cornflowerblue')
-ax.plot(mtime, mwaterlevel, color='turquoise', alpha=0.75)
-ax.set_title("San Francisco : 9414290")
-ax.legend(["obs","model"], loc='best')
-ax.set_ylabel("m")
-ax.set_xlim([dt.date(2012,10,1), dt.date(2012,10,7)])
-fig.autofmt_xdate()
-fig.savefig(savepath + "SanFrancisco.png")
-fig.savefig(savepath + "SanFrancisco", format='pdf')
 
+##         
+## Simple caching for tide data to avoid re-fetching too much.
+cache_dir=os.path.join(path,'validation_metrics',"cache")
+os.path.exists(cache_dir) or os.makedirs(cache_dir)
 
+def noaa_cached(noaa_station,product,start_date,end_date,*a,**k):
+        start_dt=utils.to_datetime(start_date)
+        end_dt = utils.to_datetime(end_date)
+        
+        cache_fn=os.path.join(cache_dir,
+                              "%s-%s-%s-%s.nc"%(noaa_station,product,
+                                                start_dt.strftime("%Y%m%d"),
+                                                end_dt.strftime("%Y%m%d")))
+        if not os.path.exists(cache_fn):
+                dat=noaa_coops.coops_dataset_product(station=noaa_station,
+                                                     product=product,
+                                                     start_date=start_date,
+                                                     end_date=end_date,
+                                                     days_per_request=31)
+                dat.to_netcdf(cache_fn)
+        # even if newly fetched, read from disk to avoid non-reproducible behavior.
+        return xr.open_dataset(cache_fn)
+        
+##
 
-
-##### station 9415020
-dat =  noaa_coops.coops_dataset_product(station="9415020", product="water_level", start_date=np.datetime64("2012-08-01"), end_date=np.datetime64("2013-09-01"), days_per_request=31)
-lon = dat["lon"].values[0,0]
-lat = dat["lat"].values[0,0]
-time = dat["time"]
-dtime = [dt.datetime.utcfromtimestamp(time[i].astype(float)/1e9) for i in range(len(time))]
-times = date2num(dtime)
-waterlevel = dat["water_level"][0,:]
-ofreq, ospec = an.band_avg(times, waterlevel, dt=times[1]-times[1])
-dist = np.sqrt((mll[0,:]-lon)**2 +  (mll[1,:]-lat)**2)
-rec = np.where(dist == np.min(dist))[0]
-mtime, mwaterlevel = wv.load_model(hisfile, rec=rec)
-mtimes = date2num(mtime)
-mwaterleveli = np.interp(times, mtimes, np.asarray(mwaterlevel)[:,0])
-mfreq, mspec = an.band_avg(mtimes, np.asarray(mwaterlevel)[:,0])
-#ms, bias, r2, rms, lag = an.model_metrics(times, mwaterleveli, times, waterlevel)
-#f.write("Point Reyes & %0.4f & %0.4f & %0.4f & %0.4f & %0.4f \\\ \\hline \n" % (ms, bias, r2, rms, lag))
-# plotting
-fig, ax = plt.subplots(figsize=(6,4))
-ax.plot(time, waterlevel, color='cornflowerblue')
-ax.plot(mtime, mwaterlevel, color='turquoise', alpha=0.75)
-ax.set_title("Point Reyes : 9415020")
-ax.legend(["obs","model"], loc='best')
-ax.set_ylabel("m")
-ax.set_xlim([dt.date(2012,10,1), dt.date(2012,10,7)])
-fig.autofmt_xdate()
-fig.savefig(savepath + "PointReyes.png")
-fig.savefig(savepath + "PointReyes", format='pdf')
-
-
-##### station 9414863
-dat =  noaa_coops.coops_dataset_product(station="9414863", product="water_level", start_date=np.datetime64("2012-08-01"), end_date=np.datetime64("2013-09-01"), days_per_request=31)
-lon = dat["lon"].values[0,0]
-lat = dat["lat"].values[0,0]
-time = dat["time"]
-dtime = [dt.datetime.utcfromtimestamp(time[i].astype(float)/1e9) for i in range(len(time))]
-times = date2num(dtime)
-waterlevel = dat["water_level"][0,:]
-ofreq, ospec = an.band_avg(times, waterlevel, dt=times[1]-times[1])
-dist = np.sqrt((mll[0,:]-lon)**2 +  (mll[1,:]-lat)**2)
-rec = np.where(dist == np.min(dist))[0]
-mtime, mwaterlevel = wv.load_model(hisfile, rec=rec)
-mtimes = date2num(mtime)
-mwaterleveli = np.interp(times, mtimes, np.asarray(mwaterlevel)[:,0])
-mfreq, mspec = an.band_avg(mtimes, np.asarray(mwaterlevel)[:,0])
-#ms, bias, r2, rms, lag = an.model_metrics(times, mwaterleveli, times, waterlevel)
-#f.write("Richmond & %0.4f & %0.4f & %0.4f & %0.4f & %0.4f \\\ \\hline \n" % (ms, bias, r2, rms, lag))
-# plotting
-fig, ax = plt.subplots(figsize=(6,4))
-ax.plot(time, waterlevel, color='cornflowerblue')
-ax.plot(mtime, mwaterlevel, color='turquoise', alpha=0.75)
-ax.set_title("Richmond : 9414863")
-ax.legend(["obs","model"], loc='best')
-ax.set_ylabel("m")
-ax.set_xlim([dt.date(2012,10,1), dt.date(2012,10,7)])
-fig.autofmt_xdate()
-fig.savefig(savepath + "Richmond.png")
-fig.savefig(savepath + "Richmond", format='pdf')
-
-
-##### station 9414750
-dat =  noaa_coops.coops_dataset_product(station="9414750", product="water_level", start_date=np.datetime64("2012-08-01"), end_date=np.datetime64("2013-09-01"), days_per_request=31)
-lon = dat["lon"].values[0,0]
-lat = dat["lat"].values[0,0]
-time = dat["time"]
-dtime = [dt.datetime.utcfromtimestamp(time[i].astype(float)/1e9) for i in range(len(time))]
-times = date2num(dtime)
-waterlevel = dat["water_level"][0,:]
-ofreq, ospec = an.band_avg(times, waterlevel, dt=times[1]-times[1])
-dist = np.sqrt((mll[0,:]-lon)**2 +  (mll[1,:]-lat)**2)
-rec = np.where(dist == np.min(dist))[0]
-mtime, mwaterlevel = wv.load_model(hisfile, rec=rec)
-mtimes = date2num(mtime)
-mwaterleveli = np.interp(times, mtimes, np.asarray(mwaterlevel)[:,0])
-mfreq, mspec = an.band_avg(mtimes, np.asarray(mwaterlevel)[:,0])
-#ms, bias, r2, rms, lag = an.model_metrics(times, mwaterleveli, times, waterlevel)
-#f.write("Alameda & %0.4f & %0.4f & %0.4f & %0.4f & %0.4f \\\ \\hline \n" % (ms, bias, r2, rms, lag))
-# plotting
-fig, ax = plt.subplots(figsize=(6,4))
-ax.plot(time, waterlevel, color='cornflowerblue')
-ax.plot(mtime, mwaterlevel, color='turquoise', alpha=0.75)
-ax.set_title("Alameda : 9414750")
-ax.legend(["obs","model"], loc='best')
-ax.set_ylabel("m")
-ax.set_xlim([dt.date(2012,10,1), dt.date(2012,10,7)])
-fig.autofmt_xdate()
-fig.savefig(savepath + "Alameda.png")
-fig.savefig(savepath + "Alameda", format='pdf')
-
-
-##### station 9414523
-dat =  noaa_coops.coops_dataset_product(station="9414523", product="water_level", start_date=np.datetime64("2012-08-01"), end_date=np.datetime64("2013-09-01"), days_per_request=31)
-lon = dat["lon"].values[0,0]
-lat = dat["lat"].values[0,0]
-time = dat["time"]
-dtime = [dt.datetime.utcfromtimestamp(time[i].astype(float)/1e9) for i in range(len(time))]
-times = date2num(dtime)
-waterlevel = dat["water_level"][0,:]
-ofreq, ospec = an.band_avg(times, waterlevel, dt=times[1]-times[1])
-dist = np.sqrt((mll[0,:]-lon)**2 +  (mll[1,:]-lat)**2)
-rec = np.where(dist == np.min(dist))[0]
-mtime, mwaterlevel = wv.load_model(hisfile, rec=rec)
-mtimes = date2num(mtime)
-mwaterleveli = np.interp(times, mtimes, np.asarray(mwaterlevel)[:,0])
-mfreq, mspec = an.band_avg(mtimes, np.asarray(mwaterlevel)[:,0])
-#ms, bias, r2, rms, lag = an.model_metrics(times, mwaterleveli, times, waterlevel)
-#f.write("Redwood City & %0.4f & %0.4f & %0.4f & %0.4f & %0.4f \\\ \\hline \n" % (ms, bias, r2, rms, lag))
-# plotting
-fig, ax = plt.subplots(figsize=(6,4))
-ax.plot(time, waterlevel, color='cornflowerblue')
-ax.plot(mtime, mwaterlevel, color='turquoise', alpha=0.75)
-ax.set_title("Redwood City : 9414523")
-ax.legend(["obs","model"], loc='best')
-ax.set_xlim([dt.date(2012,10,1), dt.date(2012,10,7)])
-fig.autofmt_xdate()
-fig.savefig(savepath + "RedwoodCity.png")
-fig.savefig(savepath + "RedwoodCity", format='pdf')
-
+f = open(metpath + met, "w")
+f.write("\\begin{center} \n")
+f.write("\\begin{adjustbox}{width=1\\textwidth} \n")
+f.write("\\begin{tabular}{| l | r | r | r | r | r | r |} \n")
 f.write("\\hline \n")
+f.write("Name             & Skill   &  Bias (m) & \(r^2\)   & RMSE (m)   & Lag (min) & Amp. factor\\\ \\hline \n")
+
+def plot_noaa_comparison(noaa_station,title,base_name,datum=''):
+        dat =  noaa_cached(noaa_station=noaa_station, product="water_level",
+                           start_date=np.datetime64("2012-08-01"),
+                           end_date=np.datetime64("2013-09-01"),
+                           days_per_request=31)
+        lon = dat["lon"].values[0,0]
+        lat = dat["lat"].values[0,0]
+        time = dat["time"]
+        # faster date conversion:
+        times=utils.to_dnum(dat.time.values)
+        waterlevel = dat["water_level"][0,:]
+
+        dist = np.sqrt((mll[0,:]-lon)**2 +  (mll[1,:]-lat)**2)
+        rec = np.where(dist == np.min(dist))[0]
+        mtime, mwaterlevel = wv.load_model(hisfile, rec=rec)
+        mtimes = date2num(mtime)
+        mwaterleveli = np.interp(times, mtimes, np.asarray(mwaterlevel)[:,0])
+        
+        ms, bias, r2, rms, lag, amp = an.model_metrics(times, mwaterleveli,
+                                                       times, waterlevel.values)
+
+        if datum!='relative':
+                bias="%0.3f"%bias
+        else:
+                bias="--"
+        # since times are datenums, lag is in decimal days.
+        f.write("%-16s & %7.3f & %9s & %7.3f & %7.3f & %6.1f & %6.2f \\\ \\hline \n" % (title,
+                                                                                        ms, bias, r2, rms,
+                                                                                        24*60*lag,
+                                                                                        amp))
+        
+        # plotting
+        if datum=='relative':
+                waterlevel = waterlevel-waterlevel.mean()
+                mwaterlevel = mwaterlevel-mwaterlevel.mean()
+        fig, ax = plt.subplots(figsize=(6,4))
+        ax.plot(time, waterlevel, color='cornflowerblue',lw=1.5)
+        # Not as pretty, but maybe more legible
+        ax.plot(mtime, mwaterlevel, color='k',lw=0.8) # color='turquoise', alpha=0.75)
+        ax.set_title("%s (%s)"%(title,noaa_station))
+        ax.legend(["Observed","Model"], loc='best')
+        if datum=='relative':
+                ax.set_ylabel("Water level anomaly (m)")
+        else:
+                ax.set_ylabel("Water level (m %s)"%datum)
+                
+        ax.set_xlim([dt.date(2012,10,1), dt.date(2012,10,7)])
+        fig.subplots_adjust(left=0.17) # text was a bit cramped.
+        fig.autofmt_xdate()
+        fig.savefig(savepath + "%s.png"%base_name)
+        fig.savefig(savepath + "%s.pdf"%base_name)
+
+#         
+plot_noaa_comparison(noaa_station="9414290",
+                     title="San Francisco",
+                     base_name="SanFrancisco",
+                     datum='NAVD88')
+
+plot_noaa_comparison(noaa_station="9415020",
+                     title="Point Reyes",
+                     base_name="PointReyes",
+                     datum='NAVD88')
+
+plot_noaa_comparison(noaa_station="9414863",
+                     title="Richmond",
+                     base_name="Richmond",
+                     datum='relative')
+
+plot_noaa_comparison(noaa_station="9414750",
+                     title="Alameda",
+                     base_name="Alameda",
+                     datum="NAVD88")
+
+plot_noaa_comparison(noaa_station="9414523",
+                     title="Redwood City",
+                     base_name="RedwoodCity",
+                     datum="relative")
+
+plot_noaa_comparison(noaa_station="9415144",
+                     title="Port Chicago",
+                     base_name="PortChicago",
+                     datum="NAVD88")
+
+
+
+# f.write("\\hline \n") # repeats
 f.write("\\end{tabular} \n")
 f.write("\\end{adjustbox} \n")
 f.write("\\end{center} \n")
 f.close()
 
+plt.close('all')
 
 #fig, ax = plt.subplots(nrows=4, sharex=True, figsize=(10,10))
 #ax[0].plot(time_1, zeta_1-np.mean(zeta_1), color='cornflowerblue')
